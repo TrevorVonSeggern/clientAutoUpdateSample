991efdb922642574724c6a97202514782572a946
Trevor Von Seggern <tvoman2@gmail.com>
2017-03-26 21:55:46 -0600

    Displays cwd.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Created by trevor on 3/26/17.
 var e = require("express");
 var fs = require("fs");
+var process = require("process");
 var express = e();
 express.get('', function (req, res) {
     var version = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8'))['version'];
-    res.json({ message: 'here...', version: version });
+    res.json({ message: 'here...', version: version, cwd: process.cwd() });
 });
 express.listen(3000);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 // Created by trevor on 3/26/17.
 import * as e from 'express';
 import * as fs from 'fs';
+import * as process from 'process';
 
 let express = e();
 
 express.get('', function (req, res) {
 	let version = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8'))['version'];
-	res.json({message: 'here...', version: version});
+	res.json({message: 'here...', version: version, cwd: process.cwd()});
 });
 
 express.listen(3000);
